c09a3fad25422b3ae8e0c84f4c1bc45df322f4ac
Mike <killeymic@gmail.com>
2026-01-03 09:24:11 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# AutoRunandWalk
+<p align="center">
+	<a href="https://www.nexusmods.com/mountandblade2bannerlord/mods/" alt="NexusMods Auto Run and Walk">
+	<img src="https://img.shields.io/badge/NexusMods-Auto Run and Walk-yellow.svg" /></a>
+</p>
+
+# Auto Run and Walk
+
+Auto Run and Walk is a simple mod for Bannerlord that walks for the player when the equals key is pressed. Disable by pressing W.
---
+++ src/AutoRunandWalk/SubModule.cs
@@ -0,0 +1,29 @@
+﻿using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.Screens;
+using TaleWorlds.ScreenSystem;
+
+namespace AutoRunandWalk
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public static bool IsAutowalk { get; set; } = false;
+
+        protected override void OnApplicationTick(float dt)
+        {
+            if (Mission.Current != null && ScreenManager.TopScreen != null)
+            {
+                if (Mission.Current.IsFieldBattle || Mission.Current.IsSiegeBattle || Mission.Current.IsNavalBattle || Mission.Current.SceneName.Contains("arena") || (MapEvent.PlayerMapEvent != null && MapEvent.PlayerMapEvent.IsHideoutBattle))
+                {
+                    MissionScreen missionScreen = ScreenManager.TopScreen as MissionScreen;
+                    if (missionScreen != null && missionScreen.InputManager != null && missionScreen.InputManager.IsKeyPressed(InputKey.Equals))
+                    {
+                        Input.PressKey(new Key(InputKey.W).InputKey);
+                        Mission.Current.SetLastMovementKeyPressed(Agent.MovementControlFlag.Forward);
+                    }
+                }
+            }
+        }
+    }
+}
